01c51b148deb382f996c95561fe0c31db49f5f62
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2016-01-15 11:31:22 +0900

    Add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = User.create([
+  { name: "Alice" },
+  { name: "Bob" },
+])
